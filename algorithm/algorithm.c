5ca53751344d06a2567e56909bebab411874b55a
benjibraun <benjibraun@gmail.com>
2018-04-19 10:23:52 +0200

    algorithm done

--- algorithm/algorithm.c
+++ algorithm/algorithm.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /* Write a function which is called "get_fibonacci_number". It returns the "N"th
  * Fibonacci number, where "N" is received as a paramter.
  * For example:
@@ -10,3 +12,32 @@
  *
  *  If the function gets invalid parameter it returns -1.
  */
+int get_fibonacci_number(int);
+ int main()
+ {
+    int fibonacci_number;
+    printf("enter number:");
+    scanf("%d",&fibonacci_number);
+    printf("%d",get_fibonacci_number(fibonacci_number));
+    return 0;
+}
+
+int get_fibonacci_number(int n)
+{
+    int current;
+    int first=0;
+    int second = 1;
+    if (n < 0){
+        return -1;
+    }
+    for (int i = 0; i < n; i++){
+        if (i <= 1){
+          current = i;
+        }else{
+          current = first + second;
+          first = second;
+          second = current;
+        }
+      }
+    return current;
+}
